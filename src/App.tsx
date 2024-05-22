a672581991027f909e37164db443c35dff62eb28
asrizvi888 <asrizvi2357@gmail.com>
2024-05-22 23:01:09 +0600

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
 
 const App = () => {
   return (
-    <>
-        <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="doctor" element={<Doctors />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
