68663e78759a1dee5490c84009d1e5824e394516
nɒvƎ ɿɘiɿʜɒʜƧ ʇɘtA <91305837+atefevan@users.noreply.github.com>
2024-05-30 20:31:54 +0600

    Ambulance Details ++

--- src/mock/strings.ts
+++ src/mock/strings.ts
@@ -38,35 +38,150 @@ export const common_concerns = [
   },
 ];
 
-export const areas =  [
-  "Barguna", "Barisal", "Bhola", "Jhalokati", "Patuakhali", "Pirojpur", // Barishal Division
-  "Bandarban", "Brahmanbaria", "Chandpur", "Chittagong", "Comilla", "Cox's Bazar", "Feni", "Khagrachari", "Lakshmipur", "Noakhali", "Rangamati", // Chattogram Division
-  "Dhaka", "Faridpur", "Gazipur", "Gopalganj", "Kishoreganj", "Madaripur", "Manikganj", "Munshiganj", "Narayanganj", "Narsingdi", "Rajbari", "Shariatpur", "Tangail", // Dhaka Division
-  "Jamalpur", "Mymensingh", "Netrakona", "Sherpur", // Mymensingh Division
-  "Bagerhat", "Chuadanga", "Jessore", "Jhenaidah", "Khulna", "Kushtia", "Magura", "Meherpur", "Narail", "Satkhira", // Khulna Division
-  "Bogura", "Joypurhat", "Naogaon", "Natore", "Chapainawabganj", "Pabna", "Rajshahi", "Sirajganj", // Rajshahi Division
-  "Dinajpur", "Gaibandha", "Kurigram", "Lalmonirhat", "Nilphamari", "Panchagarh", "Rangpur", "Thakurgaon", // Rangpur Division
-  "Habiganj", "Moulvibazar", "Sunamganj", "Sylhet" // Sylhet Division
-];;
+export const areas = [
+  "Barguna",
+  "Barisal",
+  "Bhola",
+  "Jhalokati",
+  "Patuakhali",
+  "Pirojpur", // Barishal Division
+  "Bandarban",
+  "Brahmanbaria",
+  "Chandpur",
+  "Chittagong",
+  "Comilla",
+  "Cox's Bazar",
+  "Feni",
+  "Khagrachari",
+  "Lakshmipur",
+  "Noakhali",
+  "Rangamati", // Chattogram Division
+  "Dhaka",
+  "Faridpur",
+  "Gazipur",
+  "Gopalganj",
+  "Kishoreganj",
+  "Madaripur",
+  "Manikganj",
+  "Munshiganj",
+  "Narayanganj",
+  "Narsingdi",
+  "Rajbari",
+  "Shariatpur",
+  "Tangail", // Dhaka Division
+  "Jamalpur",
+  "Mymensingh",
+  "Netrakona",
+  "Sherpur", // Mymensingh Division
+  "Bagerhat",
+  "Chuadanga",
+  "Jessore",
+  "Jhenaidah",
+  "Khulna",
+  "Kushtia",
+  "Magura",
+  "Meherpur",
+  "Narail",
+  "Satkhira", // Khulna Division
+  "Bogura",
+  "Joypurhat",
+  "Naogaon",
+  "Natore",
+  "Chapainawabganj",
+  "Pabna",
+  "Rajshahi",
+  "Sirajganj", // Rajshahi Division
+  "Dinajpur",
+  "Gaibandha",
+  "Kurigram",
+  "Lalmonirhat",
+  "Nilphamari",
+  "Panchagarh",
+  "Rangpur",
+  "Thakurgaon", // Rangpur Division
+  "Habiganj",
+  "Moulvibazar",
+  "Sunamganj",
+  "Sylhet", // Sylhet Division
+];
 
 export const ambulances = [
-  {title:"AC Ambulance",image:webp.ambulance_front,point_1:"Get ambulance within 30 minutes*",point_2:"24/7 affordable quality service",point_3:"We are just a call away: 01405600700"},
-  {title:"AIR Ambulance",image:webp.helicopter_right_side,point_1:"Get ambulance within 30 minutes*",point_2:"24/7 affordable quality service",point_3:"We are just a call away: 01405600700"},
-  {title:"ALS Ambulance",image:webp.als_ambulance_front,point_1:"Get ambulance within 30 minutes*",point_2:"24/7 affordable quality service",point_3:"We are just a call away: 01405600700"},
+  {
+    title: "AC Ambulance",
+    image: webp.ambulance_front,
+    point_1: "Get ambulance within 30 minutes*",
+    point_2: "24/7 affordable quality service",
+    point_3: "We are just a call away: 01405600700",
+  },
+  {
+    title: "AIR Ambulance",
+    image: webp.helicopter_right_side,
+    point_1: "Get ambulance within 30 minutes*",
+    point_2: "24/7 affordable quality service",
+    point_3: "We are just a call away: 01405600700",
+  },
+  {
+    title: "ALS Ambulance",
+    image: webp.als_ambulance_front,
+    point_1: "Get ambulance within 30 minutes*",
+    point_2: "24/7 affordable quality service",
+    point_3: "We are just a call away: 01405600700",
+  },
   // {title:"Freezing Ambulance Service",image:webp.freezing_ambulance_right_side,point_1:""},
   // {title:"ICU Ambulance Service",image:webp.icu_ambulance_front_1,point_1:""},
   // {title:"NICU Ambulance Service",image:webp.nicu_ambulance_front,point_1:""},
   // {title:"Non Ac Ambulance Service",image:webp.non_ac_ambulance_left_side,point_1:""},
-
 ];
 
-export const pages = ["Doctor", "Hospital", "Ambulance", "Services"];
+export const pages = ["Doctor", "Hospital", "Ambulance"];
 
 export const doctors = [
-  {title:"Dr. Abbas",image:png.d1,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"cardiology"},
-  {title:"Dr. Mariam",image:png.d2,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"gastrology"},
-  {title:"Dr.Jackson",image:png.d3,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"sexology"},
-  {title:"Dr. Krishna",image:png.d4,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"cardiology"},
-  {title:"Dr. Arnob",image:png.d5,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"cardiology"},
-  {title:"Dr. Nishat",image:png.d2,location:"Ibne Sina",degree:"MBBS Doctor",exp:"30",dept:"cardiology"},
-  ];
+  {
+    title: "Dr. Abbas",
+    image: png.d1,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "cardiology",
+  },
+  {
+    title: "Dr. Mariam",
+    image: png.d2,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "gastrology",
+  },
+  {
+    title: "Dr.Jackson",
+    image: png.d3,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "sexology",
+  },
+  {
+    title: "Dr. Krishna",
+    image: png.d4,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "cardiology",
+  },
+  {
+    title: "Dr. Arnob",
+    image: png.d5,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "cardiology",
+  },
+  {
+    title: "Dr. Nishat",
+    image: png.d2,
+    location: "Ibne Sina",
+    degree: "MBBS Doctor",
+    exp: "30",
+    dept: "cardiology",
+  },
+];
